1b8dcfdbdf88c6b7127e61cef83a055a200afcf8
James P. Howard, II <jh@jameshoward.us>
2008-07-08 20:54:40 -0500

    It's turning into a package...

---
+++ R/waterfallplot.R
@@ -0,0 +1,115 @@
+# waterfallplot.R
+
+waterfallplot <- function(height, ...) UseMethod("waterfallplot")
+
+waterfallplot.default <-
+  function(height, width=1, space=NULL, names.arg=NULL, horiz=FALSE,
+           density=NULL, angle=45, col=NULL, border=par("fg"), main=NA,
+           sub=NA, xlab=NULL, ylab=NULL, xlim=NULL, ylim=NULL, xpd=TRUE,
+           axes=TRUE, axisnames=TRUE, cex.axis=par("cex.axis"),
+           cex.names=par("cex.axis"), plot=TRUE, axis.lty=0, offset = 0,
+           add = FALSE, summary=FALSE, rev=FALSE, ...) {
+  #  Default working values
+  respaxis <- 2
+  expaxis <- 1
+  
+  #  We're going to use this a lot, so we may as well save it
+  l <- length(height)
+
+  #  Figure out the summary, and reverse direction of everything if the
+  #  summary is requested and reversed
+  if(summary==TRUE) {
+    height[l+1] = -sum(height)
+    l = l+1
+  }
+  if(rev==TRUE) {
+    height = rev(height)
+    if(summary==TRUE)
+      height = -height
+  }
+
+  if(!is.null(names.arg) && length(names.arg) != l)
+    stop("incorrect number of names")
+  if(!is.null(names(height)))
+    names.arg <- names(height)
+  
+  #  Default to 45 deg. shading lines and none, don't ask
+  density.vec <- rep(density, l, length.out = l)
+  angle.vec <- rep(angle, l, length.out = l)
+  
+  #  Default to grey boxes
+  if(is.null(col))
+    col <- "grey"
+  col.vec <- rep(col, l, length.out = l)
+  border.vec <- rep(border, l, length.out = l)
+
+  #  Assemble the baseline and topline vectors.  Default the spacing
+  #  between bars to 1/5 of the average bar width.  Create the same
+  #  initial offset as present in barplot()
+  width.vec <- rep(width, l, length.out = l)
+  if(is.null(space))
+    space <- 0.2
+  space.vec <- rep(space, l + 1, length.out = l + 1)
+  space.vec <- space.vec * mean(width.vec)
+  leftline <- rightline <- topline <- baseline <- rep(offset, l)
+  leftline[1] <- space.vec[1]
+  for(i in 1:l) {
+    topline[i] <- baseline[i] + height[i]
+    baseline[i+1] <- topline[i]
+    rightline[i] <- leftline[i] + width.vec[i]
+    leftline[i+1] <- rightline[i] + space.vec[i+1]
+  }
+  ticks.vec = rep(0, times=l)
+  
+  #  Wait, do we need to turns this on its side?  Barplot uses an
+  #  internal function that rearranges the order of arguments.  This is
+  #  a bit less hassle to do it once, though the line drawing functions
+  #  will need to be checked later.
+  if(horiz == TRUE) {
+    oldtopline <- topline
+    topline <- leftline
+    leftline <- oldtopline
+    oldbaseline <- baseline
+    baseline <- rightline
+    rightline <- oldbaseline
+    respaxis <- 1
+    expaxis <- 2
+    for(i in 1:l)
+      ticks.vec[i] <- (baseline[i] + topline[i]) / 2
+  } else {
+    for(i in 1:l)
+      ticks.vec[i] <- (leftline[i] + rightline[i]) / 2  
+  }
+  
+  #  Set up the frame
+  if(is.null(xlim))
+    xlim <- c(min(leftline), max(rightline))
+  if(is.null(ylim))
+    ylim <- c(min(topline, baseline), max(topline,baseline))
+  
+  #  Build the chart from the ground up
+  if(plot == TRUE) {
+    if(add == FALSE)
+      plot.new()
+    plot.window(xlim=xlim, ylim=ylim, ...)
+    for(i in 1:l)
+      rect(leftline[i], baseline[i], rightline[i], topline[i],
+           density = density.vec[i], angle = angle.vec[i], col=
+           col.vec[i], border=border.vec[i], xpd=xpd, ...)
+    if(horiz == TRUE)
+      for(i in 1:(l - 1))
+        lines(c(leftline[i], leftline[i]),
+              c(baseline[i], topline[i+1]), ...)
+    else
+      for(i in 1:(l - 1))
+        lines(c(rightline[i], leftline[i+1]),
+              c(topline[i], baseline[i+1]), ...)
+    if(!is.null(names.arg) && axisnames) 
+      axis(expaxis, at = ticks.vec, labels = names.arg, lty =
+           axis.lty, cex.axis = cex.names, ...)
+    if(axes == TRUE)
+      axis(respaxis, cex.axis=cex.axis, ...)
+    title(main=main, sub=sub, xlab=xlab, ylab=ylab, ...)
+  }
+  invisible(ticks.vec)
+}
---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+.First.lib <-
+    function(libname, pkgname, ...) {
+        
+        # Preload the Lattice library
+        library(package = "lattice")
+    }
