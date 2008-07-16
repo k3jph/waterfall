37acabe33a7a5099e3700f783d2ca95c7a30ff54
James P. Howard, II <jh@jameshoward.us>
2008-07-15 20:29:20 -0500

    Reformat the data files to make them a bit more clean.

--- data/rasiel.R
+++ data/rasiel.R
@@ -1,13 +1,13 @@
 ##  $Id$
-## 
+##
 ##  Ethan M. Rasiel, The McKinsey Way: Using the Techniques of the
 ##  World's Top Strategic Consultants to Help You and Your Business
-##  (New York:  McGraw-Hill, 1999), 113-118.    
+##  (New York:  McGraw-Hill, 1999), 113-118.
 
 `rasiel` <-
-structure(list(label = structure(c(3L, 4L, 2L, 1L, 5L), .Label = c("Gain on Sale", 
-"Interest Income", "Net Widget Sales", "Operating Expenses", 
-"Taxes"), class = "factor"), value = c(150L, -170L, 18L, 10L, 
--2L), subtotal = structure(c(1L, 1L, 2L, 2L, 2L), .Label = c("EBIT", 
-"Net Income"), class = "factor")), .Names = c("label", "value", 
+structure(list(label = structure(c(3L, 4L, 2L, 1L, 5L), .Label = c("Gain on Sale",
+"Interest Income", "Net Widget Sales", "Operating Expenses",
+"Taxes"), class = "factor"), value = c(150L, -170L, 18L, 10L,
+-2L), subtotal = structure(c(1L, 1L, 2L, 2L, 2L), .Label = c("EBIT",
+"Net Income"), class = "factor")), .Names = c("label", "value",
 "subtotal"), class = "data.frame", row.names = c(NA, -5L))
